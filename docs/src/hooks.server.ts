986335d4c125e19c7c31fdc25599f899f0cb0b2f
Jason Kummerl <jason.h@kummerl.com>
2025-09-20 15:45:27 -0400

    Package updates

--- docs/src/hooks.server.ts
+++ docs/src/hooks.server.ts
@@ -1,5 +1,8 @@
+import { env } from '$env/dynamic/public'
 import { paraglideMiddleware } from '$lib/paraglide/server'
+import { initCloudflareSentryHandle, sentryHandle } from '@sentry/sveltekit'
 import type { Handle } from '@sveltejs/kit'
+import { sequence } from '@sveltejs/kit/hooks'
 
 const handleParaglide: Handle = ({ event, resolve }) =>
     paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -10,4 +13,23 @@ const handleParaglide: Handle = ({ event, resolve }) =>
         })
     })
 
-export const handle: Handle = handleParaglide
+export const fullSentryHandle = sequence(
+    initCloudflareSentryHandle({
+        environment: env.PUBLIC_ENVIRONMENT ?? 'local',
+        dsn: env.PUBLIC_SENTRY_DSN,
+        // Adds request headers and IP for users, for more info visit:
+        // https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#sendDefaultPii
+        sendDefaultPii: true,
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for tracing.
+        // We recommend adjusting this value in production
+        // Learn more at
+        // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+        tracesSampleRate: 1.0,
+        // Enable logs to be sent to Sentry
+        enableLogs: true
+    }),
+    sentryHandle()
+)
+
+export const handle: Handle = sequence(fullSentryHandle, handleParaglide)
--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -11,7 +11,43 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter()
+        adapter: adapter(),
+        csp: {
+            mode: 'hash',
+            directives: {
+                'default-src': ['self'],
+                'script-src': [
+                    'self',
+                    'https://kit.fontawesome.com',
+                    'https://*.ingest.us.sentry.io',
+                    'https://*.ahrefs.com',
+                    'https://*.posthog.com',
+                    'unsafe-inline'
+                ],
+                'style-src': ['self', 'unsafe-inline', 'https://kit.fontawesome.com'],
+                'img-src': ['self', 'data:', 'https:'],
+                'font-src': [
+                    'self',
+                    'data:',
+                    'https://kit.fontawesome.com',
+                    'https://ka-p.fontawesome.com'
+                ],
+                'worker-src': ['self', 'blob:'],
+                'connect-src': ['self', 'https:'],
+                'frame-ancestors': ['none'],
+                'form-action': ['self'],
+                'base-uri': ['self'],
+                'upgrade-insecure-requests': true
+            }
+        },
+        experimental: {
+            instrumentation: {
+                server: true
+            },
+            tracing: {
+                server: true
+            }
+        }
     },
     extensions: ['.svelte', '.svx']
 }
