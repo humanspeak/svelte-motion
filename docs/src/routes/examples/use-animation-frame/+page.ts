bc0c2b4e7cf1aa43f2f73c5a17787d21171a072b
Jason Kummerl <jason.h@kummerl.com>
2025-10-04 16:30:51 -0400

    Fix page

--- docs/src/routes/examples/use-animation-frame/+page.ts
+++ docs/src/routes/examples/use-animation-frame/+page.ts
@@ -3,6 +3,6 @@ import type { PageLoad } from './$types'
 export const load: PageLoad = async () => {
     return {
         title: 'useAnimationFrame',
-        sourceUrl: 'https://motion.dev/docs/react-use-animation-frame'
+        sourceUrl: 'https://examples.motion.dev/react/use-animation-frame'
     }
 }
