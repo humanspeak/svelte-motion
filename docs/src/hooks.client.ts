14b433d246d6c191edf2eb1172b8578aef65b2bc
Jason Kummerl <jason.h@kummerl.com>
2026-06-19 12:16:12 -0400

    fix(docs): read posthog env at runtime

--- docs/src/hooks.client.ts
+++ docs/src/hooks.client.ts
@@ -1,10 +1,14 @@
-import { PUBLIC_POSTHOG_PROJECT_TOKEN } from '$env/static/public'
+import { env } from '$env/dynamic/public'
 import type { HandleClientError } from '@sveltejs/kit'
 import posthog from 'posthog-js'
 
 export async function init() {
-    posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
-        ['api_host']: 'https://t.svelte.page',
+    if (!env.PUBLIC_POSTHOG_PROJECT_TOKEN || !env.PUBLIC_POSTHOG_HOST) {
+        return
+    }
+
+    posthog.init(env.PUBLIC_POSTHOG_PROJECT_TOKEN, {
+        ['api_host']: env.PUBLIC_POSTHOG_HOST,
         ['ui_host']: 'https://us.posthog.com',
         defaults: '2026-01-30',
         ['capture_exceptions']: true
--- docs/src/lib/server/posthog.ts
+++ docs/src/lib/server/posthog.ts
@@ -1,12 +1,12 @@
-import { PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_PROJECT_TOKEN } from '$env/static/public'
+import { env } from '$env/dynamic/public'
 import { PostHog } from 'posthog-node'
 
 let posthogClient: PostHog | null = null
 
 export function getPostHogClient() {
     if (!posthogClient) {
-        posthogClient = new PostHog(PUBLIC_POSTHOG_PROJECT_TOKEN, {
-            host: PUBLIC_POSTHOG_HOST,
+        posthogClient = new PostHog(env.PUBLIC_POSTHOG_PROJECT_TOKEN, {
+            host: env.PUBLIC_POSTHOG_HOST,
             flushAt: 1,
             flushInterval: 0
         })
